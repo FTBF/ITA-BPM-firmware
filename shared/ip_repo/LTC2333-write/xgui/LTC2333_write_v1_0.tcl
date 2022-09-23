26a3e90274b0e6365fe13edc6c626b36e8cc4f37
Joe Pastika <pastika@fnal.gov>
2022-09-23 14:57:30 -0500

    Add missing sv file and update IP wrapper

--- shared/ip_repo/LTC2333-write/xgui/LTC2333_write_v1_0.tcl
+++ shared/ip_repo/LTC2333-write/xgui/LTC2333_write_v1_0.tcl
@@ -4,8 +4,10 @@ proc init_gui { IPINST } {
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
   ipgui::add_param $IPINST -name "BUSY_SIGNAL" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "BUSY_TIME" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "CLOCK_PERIOD" -parent ${Page_0}
+  set BUSY_TIME [ipgui::add_param $IPINST -name "BUSY_TIME" -parent ${Page_0}]
+  set_property tooltip {Busy Time in ns} ${BUSY_TIME}
+  set CLOCK_PERIOD [ipgui::add_param $IPINST -name "CLOCK_PERIOD" -parent ${Page_0}]
+  set_property tooltip {Clock Period in ns} ${CLOCK_PERIOD}
 
 
 }
@@ -37,6 +39,33 @@ proc validate_PARAM_VALUE.CLOCK_PERIOD { PARAM_VALUE.CLOCK_PERIOD } {
 	return true
 }
 
+proc update_PARAM_VALUE.C_S_AXI_ADDR_WIDTH { PARAM_VALUE.C_S_AXI_ADDR_WIDTH } {
+	# Procedure called to update C_S_AXI_ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_ADDR_WIDTH { PARAM_VALUE.C_S_AXI_ADDR_WIDTH } {
+	# Procedure called to validate C_S_AXI_ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_DATA_WIDTH { PARAM_VALUE.C_S_AXI_DATA_WIDTH } {
+	# Procedure called to update C_S_AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_DATA_WIDTH { PARAM_VALUE.C_S_AXI_DATA_WIDTH } {
+	# Procedure called to validate C_S_AXI_DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.N_REG { PARAM_VALUE.N_REG } {
+	# Procedure called to update N_REG when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.N_REG { PARAM_VALUE.N_REG } {
+	# Procedure called to validate N_REG
+	return true
+}
+
 
 proc update_MODELPARAM_VALUE.BUSY_SIGNAL { MODELPARAM_VALUE.BUSY_SIGNAL PARAM_VALUE.BUSY_SIGNAL } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -51,5 +80,20 @@ proc update_MODELPARAM_VALUE.BUSY_TIME { MODELPARAM_VALUE.BUSY_TIME PARAM_VALUE.
 proc update_MODELPARAM_VALUE.CLOCK_PERIOD { MODELPARAM_VALUE.CLOCK_PERIOD PARAM_VALUE.CLOCK_PERIOD } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.CLOCK_PERIOD}] ${MODELPARAM_VALUE.CLOCK_PERIOD}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_DATA_WIDTH { MODELPARAM_VALUE.C_S_AXI_DATA_WIDTH PARAM_VALUE.C_S_AXI_DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_ADDR_WIDTH { MODELPARAM_VALUE.C_S_AXI_ADDR_WIDTH PARAM_VALUE.C_S_AXI_ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.N_REG { MODELPARAM_VALUE.N_REG PARAM_VALUE.N_REG } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.N_REG}] ${MODELPARAM_VALUE.N_REG}
 }
 
